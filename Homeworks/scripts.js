0207b7ef6d18fb583b553153a283a3c61235d178
unknown <vivagas2019br@gmail.com>
2026-06-25 18:44:19 -0300

    adicionei funçoes de  audio, mais conteudos principais, um footer simples e troquei a cor de fundo

--- Homeworks/scripts.js
+++ Homeworks/scripts.js
@@ -5,3 +5,19 @@ botao.addEventListener("click", () => {
     som.currentTime = 0; // reinicia o áudio
     som.play();
 });
+
+// Pegamos a referência do arquivo de áudio pelo ID
+const musica = document.getElementById('som-background');
+
+// DEFINE O VOLUME FIXO AQUI (Ex: 0.1 = 10%, 0.5 = 50%, 1.0 = 100%)
+musica.volume = 0.3;
+
+// Função para dar o Play
+function tocarMusica() {
+  musica.play();
+}
+
+// Função para dar o Pause
+function pausarMusica() {
+  musica.pause();
+}
